8b2e957bfc091b772708c883eeea7ea9cbd60148
mohamed kamal <mkahf5103@gmail.com>
2025-05-24 22:23:25 +0400

    edit shop module

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { IPagination } from '../shared/Models/Pagination';
+import { IProduct } from '../shared/Models/Product';
 
 @Component({
   selector: 'app-shop',
@@ -7,8 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './shop.component.sass'
 })
 export class ShopComponent implements OnInit{
+  constructor(private shopService:ShopService){}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-
+products:IProduct[]
+  getAllProducts(){
+  this.shopService.getProducts().subscribe({
+    next:((value:IPagination)=>{
+this.products=value.data;
+    })
+  });
+  }
 }
