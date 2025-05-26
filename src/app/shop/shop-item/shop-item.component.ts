e1e71a34e83fbe6a63648a5d8035a9285c1cf91d
mohamed kamal <mkahf5103@gmail.com>
2025-05-26 22:49:19 +0400

    handle incoming products data from api in cards

---
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../../shared/Models/Product';
+
+@Component({
+  selector: 'app-shop-item',
+  standalone: false,
+  templateUrl: './shop-item.component.html',
+  styleUrl: './shop-item.component.css'
+})
+export class ShopItemComponent {
+@Input()  product:IProduct
+}
